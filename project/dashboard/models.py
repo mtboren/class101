680f9b8c3e8e9750931538f97840186a756c2b92
MTBoren <technology@ddayinc.com>
2017-05-31 14:05:34 -0400

    update files from completion of week 3

--- project/dashboard/models.py
+++ project/dashboard/models.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
-@python_2_unicode_compatible  # only if you need to support Python 2
+# @python_2_unicode_compatible  # only if you need to support Python 2
 class Server(models.Model):
     '''https://docs.djangoproject.com/en/1.11/ref/models/fields/#genericipaddressfield '''
     name = models.CharField(max_length=200, unique=True)
@@ -15,3 +15,7 @@ class Server(models.Model):
 
     def __str__(self):
         return '{}:{}'.format(self.name, self.ip)
+
+    # def __init__(name, ip):
+    # 	self.name = name
+    # 	self.ip = ip
--- project/dashboard/views.py
+++ project/dashboard/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+from .models import Server
+
+
+def server_list(request):
+    servers = Server.objects.all()
+    template = loader.get_template('dashboard/server_list.html')
+
+    return HttpResponse(template.render(
+        {'server_list': servers}, request
+    ))
